65b46f9166209e27d969c6ed2ed62d9673552da6
Jason Wood <jgw10713@gmail.com>
2021-06-11 14:20:09 -0700

    added color functionality that changes bg of textarea based on time of day.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,8 @@
+// Displays date and time
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-
+//save button
 $(".btn").on("click", function() {
 
     console.log("did i work");   
@@ -11,16 +12,104 @@ $(".btn").on("click", function() {
     
 });
 
+//background color of text
+function bgColor() {
+    //current time
+    var currentTime = moment().hour(); 
+    //grabbing the textarea
+    var textHour = document.getElementsByTagName("textarea");   
 
-$("#text-9").val(localStorage.getItem("hour-9"));
-$("#text-10").val(localStorage.getItem("hour-10"));
-$("#text-11").val(localStorage.getItem("hour-11"));
-$("#text-12").val(localStorage.getItem("hour-12"));
-$("#text-1").val(localStorage.getItem("hour-1"));
-$("#text-2").val(localStorage.getItem("hour-2"));
-$("#text-3").val(localStorage.getItem("hour-3"));
-$("#text-4").val(localStorage.getItem("hour-4"));
-$("#text-5").val(localStorage.getItem("hour-5"));
+    console.log(textHour);
+    console.log(currentTime);
+
+    //if statements for each time block that colors the bg with 
+    //past, present, or future style
+    if (currentTime > 9) {
+        $("#9").addClass("past");
+    } else if (currentTime >= 9 && currentTime < 10) {
+        $("#9").addClass("present");
+    } else {
+        $("#9").addClass("future");
+    };
+
+    if (currentTime > 10) {
+        $("#10").addClass("past");
+    } else if (currentTime >= 10 && currentTime < 11) {
+        $("#10").addClass("present");
+    } else {
+        $("#10").addClass("future");
+    };
+
+    if (currentTime > 11) {
+        $("#11").addClass("past");
+    } else if (currentTime >= 11 && currentTime < 12) {
+        $("#11").addClass("present");
+    } else {
+        $("#11").addClass("future");
+    };
+
+    if (currentTime > 12) {
+        $("#12").addClass("past");
+    } else if (currentTime >= 12 && currentTime < 13) {
+        $("#12").addClass("present");
+    } else {
+        $("#12").addClass("future");
+    };
+
+    if (currentTime > 13) {
+        $("#13").addClass("past");
+    } else if (currentTime >= 13 && currentTime < 14) {
+        $("#13").addClass("present");
+    } else {
+        $("#13").addClass("future");
+    };
+
+    if (currentTime > 14) {
+        $("#14").addClass("past");
+    } else if (currentTime >= 14 && currentTime < 15) {
+        $("#14").addClass("present");
+    } else {
+        $("#14").addClass("future");
+    };
+
+    if (currentTime > 15) {
+        $("#15").addClass("past");
+    } else if (currentTime >= 15 && currentTime < 16) {
+        $("#15").addClass("present");
+    } else {
+        $("#15").addClass("future");
+    };
+
+    if (currentTime > 16) {
+        $("#16").addClass("past");
+    } else if (currentTime >= 16 && currentTime < 17) {
+        $("#16").addClass("present");
+    } else {
+        $("#16").addClass("future");
+    };
+
+    if (currentTime > 17) {
+        $("#17").addClass("past");
+    } else if (currentTime >= 17 && currentTime < 18) {
+        $("#17").addClass("present");
+    } else {
+        $("#17").addClass("future");
+    };
+
+   
+};
+
+bgColor();
+//local storage
+$("#9").val(localStorage.getItem("hour-9"));
+$("#10").val(localStorage.getItem("hour-10"));
+$("#11").val(localStorage.getItem("hour-11"));
+$("#12").val(localStorage.getItem("hour-12"));
+$("#13").val(localStorage.getItem("hour-1"));
+$("#14").val(localStorage.getItem("hour-2"));
+$("#15").val(localStorage.getItem("hour-3"));
+$("#16").val(localStorage.getItem("hour-4"));
+$("#17").val(localStorage.getItem("hour-5"));
 
 
 
